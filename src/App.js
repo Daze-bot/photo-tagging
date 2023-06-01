556a0c43cbbed8fcf83566f05089e410d7b6bc33
jevon-laptop <jevon.fillette@gmail.com>
2023-06-01 19:46:34 -0400

    Add firebase to code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { initializeApp } from 'firebase/app';
 
 const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyAfwnjoAcr3Mt64TtGCd2fv30GriRzGar8",
+    authDomain: "photo-tagging-3b1cb.firebaseapp.com",
+    projectId: "photo-tagging-3b1cb",
+    storageBucket: "photo-tagging-3b1cb.appspot.com",
+    messagingSenderId: "532262825696",
+    appId: "1:532262825696:web:9d198a1131eb8a9dce6a85"
+  };
 
+  initializeApp(firebaseConfig);
 
   return (
     <BrowserRouter>

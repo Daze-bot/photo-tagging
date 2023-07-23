efe5b1af7ce5ab17bc21f5c6a35a99d97f5e5c2b
jevon-laptop <jevon.fillette@gmail.com>
2023-07-23 19:29:19 -0400

    Handle profanity on leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -3,6 +3,8 @@ import { db } from "./firebaseConfig";
 import { collection, query, getDocs, orderBy, limit } from "firebase/firestore";
 import readTime from "../utils/readTime";
 import { Link } from "react-router-dom";
+import allBadWords from "../utils/allBadWords";
+import wordExceptions from "../utils/wordExceptions";
 
 const Leaderboard = () => {
   const [times, setTimes] = useState([]);
@@ -19,11 +21,36 @@ const Leaderboard = () => {
     setTimes(tempArray);
   };
 
+  // Some very edge cases can make it through the check or be flagged false positive
+  const checkBadWords = (string) => {
+    const splitString = string.split(" ");
+    const wordsToCheck = [];
+
+    if (allBadWords.includes(string.replace(/\W/g, '').replace(/\s/g, '').toLowerCase())) {
+      return true;
+    }
+
+    splitString.forEach(word => {
+      if (allBadWords.some(x => word.toLowerCase().includes(x))) {
+        wordsToCheck.push(word);
+      }
+    });
+
+    if (wordsToCheck.every(word => wordExceptions.includes(word.replace(/\W/g, '').toLowerCase()))) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   const scoreList = times.map((time, index) => (
     <li key={index}>
       <div className="timeListItem">
         <p className="timeRank">{index + 1}</p>
-        <p className="timeName">{time.name}</p>
+        {checkBadWords(time.name)
+          ? <p className="timeName">{time.name.replace(/./g, '*')}</p>
+          : <p className="timeName">{time.name}</p>
+        }
         <p className="timeTime">{readTime(time.time)}</p>
       </div>
     </li>
